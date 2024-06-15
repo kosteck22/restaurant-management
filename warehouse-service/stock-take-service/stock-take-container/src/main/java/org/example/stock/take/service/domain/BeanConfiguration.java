bc7ab1755d5b7a68b4c7f0baa841d89e589fb99a
Kamil Ostafil <kosteck22@wp.pl>
2024-06-15 08:55:53 +0200

    added stock take container module

---
+++ warehouse-service/stock-take-service/stock-take-container/src/main/java/org/example/stock/take/service/domain/BeanConfiguration.java
@@ -0,0 +1,14 @@
+package org.example.stock.take.service.domain;
+
+import org.example.warehouse.stock.take.service.domain.StockTakeDomainService;
+import org.example.warehouse.stock.take.service.domain.StockTakeDomainServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+    @Bean
+    public StockTakeDomainService saleDomainService() {
+        return new StockTakeDomainServiceImpl();
+    }
+}
---
+++ warehouse-service/stock-take-service/stock-take-container/src/main/java/org/example/stock/take/service/domain/StockTakeServiceApplication.java
@@ -0,0 +1,17 @@
+package org.example.stock.take.service.domain;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages = { "org.example.stock.take.service.dataaccess"})
+@EntityScan(basePackages = { "org.example.stock.take.service.dataaccess"})
+@SpringBootApplication(scanBasePackages = "org.example")
+public class StockTakeServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StockTakeServiceApplication.class, args);
+    }
+
+}
